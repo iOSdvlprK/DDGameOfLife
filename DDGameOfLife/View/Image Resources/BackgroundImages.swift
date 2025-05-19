2afd594b01abcfb4d127656e6a79181943513b83
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-05-19 22:40:48 +0900

    add image resources

---
+++ DDGameOfLife/View/Image Resources/BackgroundImages.swift
@@ -0,0 +1,27 @@
+//
+//  BackgroundImages.swift
+//  DDGameOfLife
+//
+//  Created by joe on 5/19/25.
+//
+
+import SwiftUI
+
+struct BackgroundImages {
+    static let all: [ImageResource] = [
+        .autumn1,
+        .autumn2,
+        .autumn3,
+        .mountain1,
+        .mountain2,
+        .mountain3,
+        .space1,
+        .space2,
+        .underwater1,
+        .underwater2,
+        .underwater3,
+        .cyber1,
+        .cyber2,
+        .code1
+    ]
+}
