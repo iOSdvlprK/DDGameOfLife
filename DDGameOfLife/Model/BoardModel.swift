60e923fa2c72b7bc56706bfd1a8cfb9695a8defb
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-05-28 22:50:19 +0900

    finalize DesignType with the pattern editor

--- DDGameOfLife/Model/BoardModel.swift
+++ DDGameOfLife/Model/BoardModel.swift
@@ -61,36 +61,213 @@ struct BoardModel {
 }
 
 enum DesignType: String, CaseIterable {
-    case block, beehive, loaf, tub // still life
-    case blinker, toad, beacon, pentaDecathlon // oscillators
+    case block, beehive, loaf, boat, tub // still life
+    case blinker, toad, beacon, pulsar, pentaDecathlon // oscillators
     case glider, LWSS, MWSS, HWSS // space ships
     
-//    var offsetDesign: [(x: Int, y: Int)] {
-//        switch self {
-//        case .block:
-//            return [(-1,-1), (0,-1), (-1,0), (0,0)]
-//        case .beehive:
-//            return []
-//        case .loaf:
-//            return []
-//        case .tub:
-//            return []
-//        case .blinker:
-//            return []
-//        case .toad:
-//            return []
-//        case .beacon:
-//            return []
-//        case .pentaDecathlon:
-//            return []
-//        case .glider:
-//            return []
-//        case .LWSS:
-//            return []
-//        case .MWSS:
-//            return []
-//        case .HWSS:
-//            return []
-//        }
-//    }
+    var offsetDesign: [(x: Int, y: Int)] {
+        switch self {
+        // still life
+        case .block:
+            return [
+                (0, -1),
+                (1, -1),
+                (0, 0),
+                (1, 0),
+            ]
+        case .beehive:
+            return [
+                (-1, -1),
+                (0, -1),
+                (-2, 0),
+                (1, 0),
+                (-1, 1),
+                (0, 1),
+            ]
+        case .loaf:
+            return [
+                (-1, -1),
+                (0, -1),
+                (-2, 0),
+                (1, 0),
+                (-1, 1),
+                (1, 1),
+                (0, 2),
+            ]
+        case .boat:
+            return [
+                (-1, -1),
+                (0, -1),
+                (-1, 0),
+                (1, 0),
+                (0, 1),
+            ]
+        case .tub:
+            return [
+                (0, -1),
+                (-1, 0),
+                (1, 0),
+                (0, 1),
+            ]
+            
+        // oscillators
+        case .blinker:
+            return [
+                (0, -1),
+                (0, 0),
+                (0, 1),
+            ]
+        case .toad:
+            return [
+                (1, -2),
+                (-1, -1),
+                (2, -1),
+                (-1, 0),
+                (2, 0),
+                (0, 1),
+            ]
+        case .beacon:
+            return [
+                (-2, -2),
+                (-1, -2),
+                (-2, -1),
+                (-1, -1),
+                (0, 0),
+                (1, 0),
+                (0, 1),
+                (1, 1),
+            ]
+        case .pulsar:
+            return [
+                (-4, -6),
+                (-3, -6),
+                (-2, -6),
+                (2, -6),
+                (3, -6),
+                (4, -6),
+                (-6, -4),
+                (-1, -4),
+                (1, -4),
+                (6, -4),
+                (-6, -3),
+                (-1, -3),
+                (1, -3),
+                (6, -3),
+                (-6, -2),
+                (-1, -2),
+                (1, -2),
+                (6, -2),
+                (-4, -1),
+                (-3, -1),
+                (-2, -1),
+                (2, -1),
+                (3, -1),
+                (4, -1),
+                (-4, 1),
+                (-3, 1),
+                (-2, 1),
+                (2, 1),
+                (3, 1),
+                (4, 1),
+                (-6, 2),
+                (-1, 2),
+                (1, 2),
+                (6, 2),
+                (-6, 3),
+                (-1, 3),
+                (1, 3),
+                (6, 3),
+                (-6, 4),
+                (-1, 4),
+                (1, 4),
+                (6, 4),
+                (-4, 6),
+                (-3, 6),
+                (-2, 6),
+                (2, 6),
+                (3, 6),
+                (4, 6),
+            ]
+        case .pentaDecathlon:
+            return [
+                (-1, -4),
+                (0, -4),
+                (1, -4),
+                (-1, -3),
+                (1, -3),
+                (-1, -2),
+                (0, -2),
+                (1, -2),
+                (-1, -1),
+                (0, -1),
+                (1, -1),
+                (-1, 0),
+                (0, 0),
+                (1, 0),
+                (-1, 1),
+                (0, 1),
+                (1, 1),
+                (-1, 2),
+                (1, 2),
+                (-1, 3),
+                (0, 3),
+                (1, 3),
+            ]
+            
+        // spaceships
+        case .glider:
+            return [
+                (0, -1),
+                (1, 0),
+                (-1, 1),
+                (0, 1),
+                (1, 1),
+            ]
+        case .LWSS:
+            return [
+                (0, -1),
+                (1, -1),
+                (-2, 0),
+                (-1, 0),
+                (1, 0),
+                (2, 0),
+                (-2, 1),
+                (-1, 1),
+                (0, 1),
+                (1, 1),
+                (-1, 2),
+                (0, 2),
+            ]
+        case .MWSS:
+            return [
+                (-2, -2),
+                (-1, -2),
+                (0, -2),
+                (1, -2),
+                (2, -2),
+                (-3, -1),
+                (2, -1),
+                (2, 0),
+                (-3, 1),
+                (1, 1),
+                (-1, 2),
+            ]
+        case .HWSS:
+            return [
+                (-3, -2),
+                (-2, -2),
+                (-1, -2),
+                (0, -2),
+                (1, -2),
+                (2, -2),
+                (-4, -1),
+                (2, -1),
+                (2, 0),
+                (-4, 1),
+                (1, 1),
+                (-2, 2),
+                (-1, 2),
+            ]
+        }
+    }
 }
