b28946b7977588ec823e8df2c0842200932cce97
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-06-09 23:25:51 +0900

    ControlButtonType enumeration

--- DDGameOfLife/View/ControlView.swift
+++ DDGameOfLife/View/ControlView.swift
@@ -27,6 +27,28 @@ struct ControlView: View {
     }
 }
 
+enum ControlButtonType {
+    case playPause(isPlaying: Bool)
+    case refresh, clear, toggleImage, changeImage, toggleVisibility(isHidden: Bool)
+    
+    var systemImage: String {
+        switch self {
+        case .playPause(let isPlaying):
+            isPlaying ? "pause" : "play"
+        case .refresh:
+            "arrow.counterclockwise"
+        case .clear:
+            "clear"
+        case .toggleImage:
+            "photo.on.rectangle"
+        case .changeImage:
+            "arrow.triangle.2.circlepath"
+        case .toggleVisibility(let isHidden):
+            isHidden ? "ladybug" : "ladybug.slash"
+        }
+    }
+}
+
 struct ButtonView: View {
     let name: String
     let font: Font = .headline
